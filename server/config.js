d7aa77f07def56d6c1e40f4cf4fb3c80f2fb00c1
felixjin <dlyujin@cn.ibm.com>
2016-08-04 14:55:47 +0800

    Initial commit

---
+++ server/config.js
@@ -0,0 +1,17 @@
+/* 
+* @Author: 
+* @Date:   2015-06-09 11:13:06
+* @Last Modified by:   chenhao
+* @Last Modified time: 2015-06-09 11:17:05
+*/
+
+module.exports = {
+    mysql_dev: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'test',
+        connectionLimit: 10,
+        supportBigNumbers: true
+    }
+};
